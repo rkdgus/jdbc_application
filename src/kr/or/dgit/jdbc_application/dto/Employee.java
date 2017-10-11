7f52972a34aef28b236b0ddf7c2217c77d2a1e6e
KangHyun Lee <rkd7327@naver.com>
2017-10-11 14:18:35 +0900

    dto생성완료

---
+++ src/kr/or/dgit/jdbc_application/dto/Employee.java
@@ -0,0 +1,78 @@
+package kr.or.dgit.jdbc_application.dto;
+
+public class Employee {
+	private int empNo;
+	private String empName;
+	private Title title;
+	private Employee manager;
+	private int salary;
+	private Department dno;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int empNo, String empName, Title title, Employee manager, int salary, Department dno) {
+		this.empNo = empNo;
+		this.empName = empName;
+		this.title = title;
+		this.manager = manager;
+		this.salary = salary;
+		this.dno = dno;
+	}
+
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Title getTitle() {
+		return title;
+	}
+
+	public void setTitle(Title title) {
+		this.title = title;
+	}
+
+	public Employee getManager() {
+		return manager;
+	}
+
+	public void setManager(Employee manager) {
+		this.manager = manager;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Department getDno() {
+		return dno;
+	}
+
+	public void setDno(Department dno) {
+		this.dno = dno;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s, %s, %s, %s, %s, %s", empNo, empName, title.getTitleName(), manager.getEmpName(),
+				salary, dno.getDeptName());
+	}
+
+}
---
+++ src/kr/or/dgit/jdbc_application/dto/Title.java
@@ -0,0 +1,37 @@
+package kr.or.dgit.jdbc_application.dto;
+
+public class Title {
+	private int titleNo;
+	private String titleName;
+
+	public Title() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Title(int titleNo, String titleName) {
+		this.titleNo = titleNo;
+		this.titleName = titleName;
+	}
+
+	public int getTitleNo() {
+		return titleNo;
+	}
+
+	public void setTitleNo(int titleNo) {
+		this.titleNo = titleNo;
+	}
+
+	public String getTitleName() {
+		return titleName;
+	}
+
+	public void setTitleName(String titleName) {
+		this.titleName = titleName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Title [titleNo=%s, titleName=%s]", titleNo, titleName);
+	}
+
+}
